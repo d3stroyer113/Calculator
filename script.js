a782854915736ff79dfb1a66110e04062a57a3fe
d3stroyer113 <119396048+d3stroyer113@users.noreply.github.com>
2023-03-28 08:48:18 +0300

    kkads kārtējais variants

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const plusins = document.querySelector
-('.js-plus')
-const inputins = document.querySelector
-('.js-input')
-console.log(plusins)
-console.log(inputins)
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
+
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
