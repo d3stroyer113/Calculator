1b031011d2ea2fd40774f20cb87e2fd01e201e43
d3stroyer113 <119396048+d3stroyer113@users.noreply.github.com>
2023-04-04 08:42:03 +0300

    Add files via upload
    moreeeeeee js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const plusins = document.querySelector('.js-plus');
-const inputins = document.querySelector('.js-input');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const devide = document.querySelector('.js-devide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result')
+
+let firstvalue = inputins1.value
+let secondvalue = inputins2.value
 
 const Add = (cipars1, cipars2) => {
     return cipars1 + cipars2
@@ -18,5 +26,5 @@ const Divide = (cipars1, cipars2) => {
 }
 
 plusins.addEventListener('click', () => {
-    console.log("Mani nospieda")
+    console.log(add(firstValue,secondValue))
 })
