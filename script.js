61e1ec7ada995a1749eea85b4058f6498ffb27ed
d3stroyer113 <119396048+d3stroyer113@users.noreply.github.com>
2023-04-11 10:06:38 +0300

    VEL JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const plusins = document.querySelector('.js-plus');
 const multiply = document.querySelector('.js-multiply');
 const minus = document.querySelector('.js-minus');
@@ -6,8 +7,6 @@ const inputins1 = document.querySelector('.js-input1');
 const inputins2 = document.querySelector('.js-input2');
 const answer = document.querySelector('.js-result')
 
-let firstvalue = inputins1.value
-let secondvalue = inputins2.value
 
 const Add = (cipars1, cipars2) => {
     return cipars1 + cipars2
@@ -26,5 +25,10 @@ const Divide = (cipars1, cipars2) => {
 }
 
 plusins.addEventListener('click', () => {
-    console.log(add(firstValue,secondValue))
+   answer.textContent = Add(parseint(inputins1.Value),parseint(inputins2.Value))
+
+   inputins1.value = ""
+   inputins2.value = ""
 })
+
+
